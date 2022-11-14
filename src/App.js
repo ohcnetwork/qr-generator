e55ff707b85bbf3a2272f60c0fe3534e76aa9c85
Aryan Patel <patelaryan7751@gmail.com>
2022-11-15 00:56:35 +0530

    Added a visible loader between the genration and download time

--- src/App.js
+++ src/App.js
@@ -154,14 +154,26 @@ function App() {
               <div class="my-4 text-sm text-gray-500">
                 Bear with us as we generate your QRs :)
               </div>
-              <div class="w-full h-6 bg-gray-200 rounded-full">
-                <div
-                  class="h-6 bg-blue-600 rounded-full"
-                  style={{
-                    width: `${((currentQR.data - start + 1) / range) * 100}%`,
-                  }}
-                ></div>
-              </div>
+              <svg
+                class="animate-spin -ml-1 mr-3 h-10 w-10 text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
             </>
           )}
 
