5e542486e28fdb2e870197f816c6558b60993079
Aakash Singh <mail@singhaakash.dev>
2022-11-15 23:32:37 +0530

    specify development server port in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { build, defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @type {import('vite').UserConfig} */
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
-    outDir: 'build',
-  }
+    outDir: "build",
+  },
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
 })
